0195cf6e78f10bdbd983d5b26c62dbb9615159ca
Martin Kielhorn <martin.kielhorn@tassinternational.com>
2017-06-22 16:29:44 +0200

    kind of compiles

--- gen.lisp
+++ gen.lisp
@@ -56,8 +56,12 @@ is replaced with replacement."
 	       (include <Magnum/Buffer.h>)
 	       (include <Magnum/Mesh.h>)
 	       (include <Magnum/Shaders/Shaders.h>)
-	       (include <Magnum/Shaders/VertexColor.h>)
+	       (include <Magnum/Shaders/Phong.h>)
 	       (include <Magnum/Math/Color.h>)
+	       (include <Magnum/Primitives/Cube.h>)
+	       (include <Magnum/MeshTools/CompressIndices.h>)
+	       (include <Magnum/MeshTools/Interleave.h>)
+	       (include <Magnum/Trade/MeshData3D.h>)
 	       (include <array>)
 	       (raw "using namespace std;")
 	       (raw "using namespace Magnum;")
@@ -68,9 +72,20 @@ is replaced with replacement."
 		      (function (MyApplication ((arguments :type "const Arguments&")) explicit))
 		      (access-specifier private)
 		      (function (drawEvent () void :specifier override))
-		      (decl ((m_buffer :type Buffer)
+		      (function (mousePressEvent ((event :type MouseEvent&)) void :specifier override))
+		      (function (mouseReleaseEvent ((event :type MouseEvent&)) void :specifier override))
+		      (function (mouseMoveEvent ((event :type MouseMoveEvent&)) void :specifier override))
+		      
+		      (decl ((m_index_buffer :type Buffer)
+			     (m_vertex_buffer :type Buffer)
 			     (m_mesh :type Mesh)
-			     (m_shader :type "Shaders::VertexColor2D"))))
+			     (m_shader :type "Shaders::Phong")
+
+			     (m_transformation :type Matrix4)
+			     (m_projection :type Matrix4)
+
+			     (m_previousMousePosition :type Vector2i)
+			     (m_color :type Color3))))
 
 	       (struct TriangleVertex ()
 		       (decl ((position :type Vector2)
@@ -83,7 +98,7 @@ is replaced with replacement."
 							    `(list (list ,x ,y) ,(format nil "0x~6,'0x_srgbf" color)))))))))
 	       
 	       
-	       ,@(dox :brief ""
+	       #+nil ,@(dox :brief ""
 		      :usage ""
 		      :params '((arguments "")
 				)
@@ -96,26 +111,63 @@ is replaced with replacement."
 	       (function ("MyApplication::MyApplication" ((arguments :type "const Arguments&")) nil
 							 :ctor (("Platform::Application" arguments)))
 			 (funcall "Renderer::setClearColor" (funcall "Color3::fromHsv" (raw "216.0_degf") .85s0 1s0))
-			 (funcall m_buffer.setData data "BufferUsage::StaticDraw")
+			 (funcall "Renderer::enable" "Renderer::Feature::DepthTest")
+			 (funcall "Renderer::enable" "Renderer::Feature::FaceCulling")
+			 (let ((cube :type "Trade::MeshData3D" :ctor (funcall "Primitives::Cube::solid")))
+			   #+nil (setf cube (funcall "Primitives::Cube::solid"))
+			   (funcall m_vertex_buffer.setData
+				    (funcall "MeshTools::interleave"
+					     (funcall cube.positions 0)
+					     (funcall cube.normals 0))
+				    "BufferUsage::StaticDraw")
+			   (let ((index_data :type "Containers::Array<char>")
+				 (index_type :type "Mesh::IndexType")
+				 (index_start :type UnsignedInt)
+				 (index_end :type UnsignedInt))
+			     (setf (funcall tie index_data index_type index_start index_end)
+				   (funcall "MeshTools::compressIndices" (funcall cube.indices)))
+			     (funcall m_index_buffer.setData index_data "BufferUsage::StaticDraw")
 
-			 ,@(loop for (e f) in `((setPrimitive ("MeshPrimitive::Triangles"))
-						(setCount (3))
-						(addVertexBuffer (m_buffer 0 "Shaders::VertexColor2D::Position{}" "Shaders::VertexColor2D::Color{Shaders::VertexColor2D::Color::Components::Three}")))
+			     ,@(loop for (e f) in `((setPrimitive ((funcall cube.primitive)))
+						(setCount ((funcall "cube.indices().size")))
+						(addVertexBuffer (m_vertex_buffer 0 "Shaders::Phong::Position{}" "Shaders::Phong::Normal{}"))
+						(setIndexBuffer (m_index_buffer 0 index_type index_start index_end)))
 			      collect
-				`(funcall (slot-value m_mesh ,e) ,@f))
-			 #+nil
-			 (raw "m_mesh.setPrimitive(MeshPrimitive::Triangles).setCount(3).addVertexBuffer(m_buffer,0,Shaders::VertexColor2D::Position{},Shaders::VertexColor2D::Color{Shaders::VertexColor2D::Color::Components::Three})")
+				`(funcall (slot-value m_mesh ,e) ,@f))))
+
+			 (setf m_transformation (* (funcall "Matrix4::rotationX" "30.0_degf")
+						   (funcall "Matrix4::rotationY" "40.0_degf"))
+			       m_color (funcall "Color3::fromHsv" "35.0_degf" 1s0 1s0)
+			       m_projection (* (funcall "Matrix4::perspectiveProjection"
+						      "35.0_degf"
+						      (funcall "Vector2{defaultFramebuffer.viewport().size()}.aspectRatio")
+						      .01s0 100s0)
+					       (funcall "Matrix4::translation"
+							(funcall "Vector3::zAxis" -10s0))))
 			 
-			 #+nil
-			 (funcall addVertexBuffer
-				  m_buffer 0 "Shaders::VertexColor2D::Position{}"  "Shaders::VertexColor2D::Color{Shaders::VertexColor2D::Color::Components::Three}")
+			 
+			 
 			 (<< (funcall Debug)
 			     (string "running on: ") (funcall "Context::current().version")
 			     (string " using ") (funcall "Context::current().rendererString")))
 
 	       (function ("MyApplication::drawEvent" () void)
 			  (funcall defaultFramebuffer.clear
-				   "FramebufferClear::Color")
+				   (|\|| "FramebufferClear::Color"
+					 "FramebufferClear::Depth"))
+			  ,@(loop for (e f) in `((setLightPosition ((list 7s0 5s0 2.5s0)))
+						 (setLightColor ("Color3{1.0f}"))
+						 (setDiffuseColor (m_color))
+						 (setAmbientColor ((funcall "Color3::fromHsv"
+									    (funcall m_color.hue)
+									    1s0
+									    .3s0)))
+						 (setTransformationMatrix (m_transformation))
+						 (setNormalMatrix ((funcall m_transformation.rotationScaling)))
+						 (setProjectionMatrix (m_projection)))
+			      collect
+				 `(funcall (slot-value m_shader ,e) ,@f))
+			  
 			  (funcall m_mesh.draw m_shader)
 			  (funcall swapBuffers))
 	       (funcall MAGNUM_APPLICATION_MAIN MyApplication)
--- source/main.cpp
+++ source/main.cpp
@@ -3,10 +3,14 @@
 #include <Magnum/DefaultFramebuffer.h>
 #include <Magnum/Math/Color.h>
 #include <Magnum/Mesh.h>
+#include <Magnum/MeshTools/CompressIndices.h>
+#include <Magnum/MeshTools/Interleave.h>
 #include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/Primitives/Cube.h>
 #include <Magnum/Renderer.h>
+#include <Magnum/Shaders/Phong.h>
 #include <Magnum/Shaders/Shaders.h>
-#include <Magnum/Shaders/VertexColor.h>
+#include <Magnum/Trade/MeshData3D.h>
 #include <Magnum/Version.h>
 #include <array>
 #include <fstream>
@@ -20,9 +24,17 @@ public:
 
 private:
   void drawEvent() override;
-  Buffer m_buffer;
+  void mousePressEvent(MouseEvent &event) override;
+  void mouseReleaseEvent(MouseEvent &event) override;
+  void mouseMoveEvent(MouseMoveEvent &event) override;
+  Buffer m_index_buffer;
+  Buffer m_vertex_buffer;
   Mesh m_mesh;
-  Shaders::VertexColor2D m_shader;
+  Shaders::Phong m_shader;
+  Matrix4 m_transformation;
+  Matrix4 m_projection;
+  Vector2i m_previousMousePosition;
+  Color3 m_color;
 };
 
 struct TriangleVertex {
@@ -34,27 +46,53 @@ array<TriangleVertex, 3> data({{{{(-5.e-1f), (-5.e-1f)}, 0xFF0000_srgbf},
                                 {{(5.e-1f), (-5.e-1f)}, 0x00FF00_srgbf},
                                 {{(0.0e+0f), (5.e-1f)}, 0x0000FF_srgbf}}});
 
-//! @brief
-//!
-//! @usage
-//!
-//! @param arguments
-//!
-//! @return
 MyApplication::MyApplication(const Arguments &arguments)
     : Platform::Application(arguments) {
   Renderer::setClearColor(Color3::fromHsv(216.0_degf, (8.5e-1f), (1.e+0f)));
-  m_buffer.setData(data, BufferUsage::StaticDraw);
-  m_mesh.setPrimitive(MeshPrimitive::Triangles);
-  m_mesh.setCount(3);
-  m_mesh.addVertexBuffer(m_buffer, 0, Shaders::VertexColor2D::Position{},
-                         Shaders::VertexColor2D::Color{
-                             Shaders::VertexColor2D::Color::Components::Three});
+  Renderer::enable(Renderer::Feature::DepthTest);
+  Renderer::enable(Renderer::Feature::FaceCulling);
+  {
+    Trade::MeshData3D cube(Primitives::Cube::solid());
+    m_vertex_buffer.setData(
+        MeshTools::interleave(cube.positions(0), cube.normals(0)),
+        BufferUsage::StaticDraw);
+    {
+      Containers::Array<char> index_data;
+      Mesh::IndexType index_type;
+      UnsignedInt index_start;
+      UnsignedInt index_end;
+      tie(index_data, index_type, index_start, index_end) =
+          MeshTools::compressIndices(cube.indices());
+      m_index_buffer.setData(index_data, BufferUsage::StaticDraw);
+      m_mesh.setPrimitive(cube.primitive());
+      m_mesh.setCount(cube.indices().size());
+      m_mesh.addVertexBuffer(m_vertex_buffer, 0, Shaders::Phong::Position{},
+                             Shaders::Phong::Normal{});
+      m_mesh.setIndexBuffer(m_index_buffer, 0, index_type, index_start,
+                            index_end);
+    }
+  }
+  m_transformation =
+      (Matrix4::rotationX(30.0_degf) * Matrix4::rotationY(40.0_degf));
+  m_color = Color3::fromHsv(35.0_degf, (1.e+0f), (1.e+0f));
+  m_projection =
+      (Matrix4::perspectiveProjection(
+           35.0_degf,
+           Vector2{defaultFramebuffer.viewport().size()}.aspectRatio(),
+           (1.e-2f), (1.e+2f)) *
+       Matrix4::translation(Vector3::zAxis((-1.e+1f))));
   (Debug() << "running on: " << Context::current().version() << " using "
            << Context::current().rendererString());
 }
 void MyApplication::drawEvent() {
-  defaultFramebuffer.clear(FramebufferClear::Color);
+  defaultFramebuffer.clear((FramebufferClear::Color | FramebufferClear::Depth));
+  m_shader.setLightPosition({(7.e+0f), (5.e+0f), (2.5e+0f)});
+  m_shader.setLightColor(Color3{1.0f});
+  m_shader.setDiffuseColor(m_color);
+  m_shader.setAmbientColor(Color3::fromHsv(m_color.hue(), (1.e+0f), (3.e-1f)));
+  m_shader.setTransformationMatrix(m_transformation);
+  m_shader.setNormalMatrix(m_transformation.rotationScaling());
+  m_shader.setProjectionMatrix(m_projection);
   m_mesh.draw(m_shader);
   swapBuffers();
 }
